61aa7fbb3efb5dab60eef9291fdc2c7770a5511f
Kyrylo Toporikov <tkyrylo@gmail.com>
2016-08-17 19:39:09 +0300

    add properties

--- G18Project/ToporikovProj/src/main/java/pages/BasePage.java
+++ G18Project/ToporikovProj/src/main/java/pages/BasePage.java
@@ -1,7 +1,6 @@
 package pages;
 
-/**
- * Created by ktoporikov on 15.08.16.
- */
 public class BasePage {
+
+
 }
--- G18Project/ToporikovProj/src/test/java/com/booking/LoginSuit.java
+++ G18Project/ToporikovProj/src/test/java/com/booking/LoginSuit.java
@@ -1,7 +1,6 @@
 package com.booking;
 
-/**
- * Created by ktoporikov on 15.08.16.
- */
 public class LoginSuit {
+
+
 }
