acf2029e8009674186dcbeedcce93d94075f8e49
Arif <huseinov.arif@gmail.com>
2016-08-21 10:58:16 +0300

    Assert updated

--- G18Project/GuseinovProj/src/test/java/mainPack/NegativeTestLoginPage.java
+++ G18Project/GuseinovProj/src/test/java/mainPack/NegativeTestLoginPage.java
@@ -1,8 +1,10 @@
 package mainPack;
 
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,20 +13,27 @@ import java.util.concurrent.TimeUnit;
 public class NegativeTestLoginPage {
 
     WebDriver driver = new ChromeDriver();
+    Logger log = Logger.getLogger(getClass());
 
     @Test
 
     public void negativeTestLoginPage(){
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        System.out.println("Window opened and maximized");
+        log.info("Window opened and maximized");
         driver.get("http://v3.test.itpmgroup.com/login");
-        System.out.println("Url is opened");
+        log.info("Url is opened");
         driver.findElement(By.xpath(".//button[@type='submit']")).click();
-        System.out.println("Button 'Вход' clicked");
-        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='login-box-msg']")).isDisplayed());
-        System.out.println("We are on login page");
+        log.info("Button 'Вход' clicked");
+        boolean  isDispl;
+        try{
+            isDispl = driver.findElement(By.xpath("//p[@class='login-box-msg']")).isDisplayed();
+        }catch(NoSuchElementException e){
+            isDispl = false;
+        }
+        Assert.assertTrue("Element was not found on the page!",isDispl);
+        log.info("We are on login page");
         driver.quit();
-        System.out.println("Driver closed");
+        log.info("Driver closed");
     }
 }
