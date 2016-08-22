c3c49d7b36c9f7a018fe17f8e821376d3edaae06
Radulenko <radulenko@gmail.com>
2016-08-23 00:21:26 +0300

    were added tests

---
+++ G18Project/RadulenkoTaniaProj/src/main/java/mainPack/Pages/LoginPage.java
@@ -0,0 +1,94 @@
+package mainPack.Pages;
+
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginPage {
+    WebDriver driver;
+    Logger log;
+
+    @FindBy(name = "_username")
+    WebElement inputLogin;
+
+    @FindBy(id = "password")
+    WebElement inputPassword;
+
+    @FindBy(xpath = ".//button[@type='submit']")
+    WebElement buttonSubmit;
+
+    @FindBy(xpath = ".//img[contains(@alt,'student')]")
+    WebElement avatar;
+
+
+    public LoginPage (WebDriver driver) {
+        this.driver = driver;
+        log =  Logger.getLogger(getClass());
+        PageFactory.initElements(driver,this);
+    }
+
+    public void openBrowserAndLoginPage(){
+        try {
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            driver.get("http://v3.test.itpmgroup.com/login");
+            log.info("Browser and Login page were opened");
+        }catch (Exception e){
+            log.error("Can not work with browser or LoginPage" + e);
+//            Assert.fail("Can not work with browser or LoginPage")
+        }
+    }
+
+    public void closeLoginPageAndBrowser (){
+        driver.quit();
+        log.info("Browser and PageLogin were closed");
+    }
+
+    public boolean isLoginPageLoad (){
+        try {
+            boolean isElementPresent =  inputPassword.isDisplayed();
+
+            return isElementPresent;
+        }catch (NoSuchElementException e){
+            log.info("No such element");
+            return false;
+
+        }
+    }
+
+    public LoginPage inputTextIntoLoginField(String text){
+        inputLogin.clear();
+        inputLogin.sendKeys(text);
+        log.info(text + " was inputed");
+        return new LoginPage(driver);
+
+    }
+
+    public LoginPage inputTextIntoPassField(String pass){
+        inputPassword.clear();
+        inputPassword.sendKeys(pass);
+        log.info(pass + "was inputed");
+        return new LoginPage(driver);
+    }
+
+
+    public void clickButtonVhod(){
+        buttonSubmit.click();
+        log.info("Button VHOD was clicked");
+    }
+
+}
+
+
+
+
+
