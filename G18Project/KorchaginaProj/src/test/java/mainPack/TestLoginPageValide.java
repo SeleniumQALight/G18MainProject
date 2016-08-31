d7fd3fedbae3493b76e6c0e9447e28f537b00dd1
Vika <viktoria_korchagina@mail.ru>
2016-08-31 20:46:08 +0300

    Valide

---
+++ G18Project/KorchaginaProj/src/test/java/mainPack/TestLoginPageValide.java
@@ -0,0 +1,41 @@
+package mainPack;
+
+//import org.apache.log4j.Logger;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestLoginPageValide {
+    WebDriver driver = new ChromeDriver();
+    Logger log = Logger.getLogger(getClass());
+
+    @Test
+    public void testLoginPageValid() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        log.info("Driver was opened and maximize");
+
+        driver.get("http://v3.test.itpmgroup.com/login");
+        log.info("Url was opened");
+
+        Assert.assertTrue("Assert works", driver.findElement(By.id("password1")).isDisplayed());
+
+        driver.quit();
+        log.info("Driver was closed");
+
+    }
+
+}
