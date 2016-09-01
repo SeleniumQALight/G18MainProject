285a16e5ac56eb8d46547e28510e7d07554fd49b
Vika <viktoria_korchagina@mail.ru>
2016-09-01 22:41:12 +0300

    App1

---
+++ G18Project/KorchaginaProj/src/main/java/mainPack/Pages/App1.java
@@ -0,0 +1,22 @@
+package mainPack.Pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Vika on 31.08.2016.
+ */
+public class App1 {
+    WebDriver driver;
+   public Apparat apparat;
+    public ApparatEdit apparatEdit;
+    public HomePage homePage;
+    public LoginPage loginPage;
+
+    public App1(WebDriver driver){
+        this.driver = driver;
+        apparat = new Apparat(driver);
+        apparatEdit = new ApparatEdit(driver);
+        homePage = new HomePage(driver);
+        loginPage = new LoginPage(driver);
+    }
+}
