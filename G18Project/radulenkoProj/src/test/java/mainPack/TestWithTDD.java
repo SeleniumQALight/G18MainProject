b6ad6b7f6b8b8a5bb028aaba5e78ae6df504123a
Radulenko <radulenko@gmail.com>
2016-09-04 23:24:31 +0300

    Added Double value to test data

--- G18Project/radulenkoProj/src/test/java/mainPack/TestWithTDD.java
+++ G18Project/radulenkoProj/src/test/java/mainPack/TestWithTDD.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 import java.io.FileInputStream;
@@ -21,12 +23,15 @@ public class TestWithTDD extends Assert{
     Logger log;
     private String firstName;
     private String lastName;
+    private double valueOfDouble;
+    WebDriver driver;
 
 
-    public TestWithTDD(String firstName, String lastName) {
+    public TestWithTDD(String firstName, String lastName, double valueOfDouble) {
         super();
         this.firstName = firstName;
         this.lastName = lastName;
+        this.valueOfDouble = valueOfDouble;
         log = Logger.getLogger(getClass());
 
     }
@@ -34,13 +39,18 @@ public class TestWithTDD extends Assert{
     @Parameters
     public static Collection testData() throws IOException {
                 InputStream spreadsheet = new FileInputStream("src/main/java/testData.xls");
-                return new SpreadsheetData(spreadsheet,"test2").getData();
+                return new SpreadsheetData(spreadsheet,"test1").getData();
     }
 
     @Test
     public void test1(){
+        driver = new ChromeDriver();
+        driver.get("http:/qalight.com.ua");
+        log.info("driver was opened");
+        driver.quit();
         log.info("firstName = " + firstName);
         log.info("lastName = " + lastName);
+        log.info("Double = " + valueOfDouble);
     }
 
     @After
